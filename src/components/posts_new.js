b1baac60e7765fec1722c01707f4550857f4fb1e
Sean Liu <sean.liu@myob.com>
2017-12-27 16:08:35 +1100

    finish CRUD of the blog posting app

---
+++ src/components/posts_new.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from "../actions";
+
+class PostsNew extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        <div className='text-help'>
+          {touched ? error : ''}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field label="Categories" name="categories" component={this.renderField} />
+        <Field label="Post Content" name="content" component={this.renderField} />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Title is mandatory!";
+  }
+  if (!values.categories) {
+    errors.categories = "Categories are mandatory!";
+  }
+  if (!values.content) {
+    errors.content = "Content is mandatory!";
+  }
+
+  // form is invalid if there is any property in errors object
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(
+  connect(null, { createPost })(PostsNew)
+  );
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from 'redux-promise';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from "./components/posts_index";
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'));
 
